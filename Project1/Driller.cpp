1f70bed34b6e05ad5a8390c08da285e75ee6cce3
Austin Green <austin.o.green-1@ou.edu>
2019-10-02 17:24:00 -0500

    Fixes some sorting to make work in all cases

--- Project1/Driller.cpp
+++ Project1/Driller.cpp
@@ -27,18 +27,23 @@ ResizableArray<DrillingRecord>* drillingArray = NULL;
 
 // Only sorts unsorted part of array
 void sortDrillingRecords(int prevSize) {
+	// Goes through only previously unsorted part of the array to the end
+	// This is where new data lies
 	for (int i = prevSize; i < drillingArray->getSize(); i++) {
 
+		// Places the new element into dR and stores time
 		DrillingRecord dR = drillingArray->get(i);
 		std::string time = dR.getString(1);
 
 		int j;
 		// Searches while it is smaller than previous size and less than time
 		// if previously not sorted, new elements are sorted in order
-		for (j = 0; ((j < prevSize) && (time < drillingArray->get(j).getString(1))); j++) {	}
+		for (j = 0; ((j < prevSize) && (time.compare(drillingArray->get(j).getString(1))) < 1); j++) {	}
 
-		// Decrements so that adds at the right place
+		// Places new string in its place
 		drillingArray->addAt(dR, j);
+		// Removes from previous place
+		drillingArray->removeAt(i);
 	}
 }
 
