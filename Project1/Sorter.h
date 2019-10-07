6362895fdefd3cc5f99661d62f5b5f220c0388bf
Austin Green <austin.o.green-1@ou.edu>
2019-10-07 15:05:14 -0500

    implemented sorter

--- Project1/Sorter.h
+++ Project1/Sorter.h
@@ -15,26 +15,26 @@ public:
     static void sort(ResizableArray<T>& array, const Comparator<T>& comparator);
 };
 
+
+// TODO: Make more efficient
 template<typename T>
 void Sorter<T> :: sort(ResizableArray<T>& array, const Comparator<T>& comparator) {
-	// Goes through only previously unsorted part of the array to the end
-	// This is where new data lies
-	for (int i = prevSize; i < drillingArray->getSize(); i++) {
+	T temp;
+	for (int i = 0; i < n; i++) {
 
-		// Places the new element into dR and stores time
-		DrillingRecord dR = drillingArray->get(i);
-		std::string time = dR.getString(1);
+		for (int j = i; j < n; j++) {
 
-		int j;
-		// Searches while it is smaller than previous size and less than time
-		// if previously not sorted, new elements are sorted in order
-		for (j = 0; ((j < prevSize) && (time.compare(drillingArray->get(j).getString(1))) < 1); j++) {}
+			// If the element in i is less than the element in j, switches the two
+			if ((comparator.compare(array[i],array[j])) < 0) {
 
-		// Places new string in its place
-		drillingArray->addAt(dR, j);
-		// Removes from previous place
-		drillingArray->removeAt(i);
+				temp = array[i];
+				array[i] = array[j];
+				array[j] = temp;
+			}
+		}
 	}
+
+	return;
 }
 
 #endif
